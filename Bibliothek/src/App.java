d8d064de4c3a72f95e91f64fa42364bb9ec86655
LokiSTR <90755864+LokiSTR@users.noreply.github.com>
2023-03-24 11:53:21 +0100

    Canvas

--- Bibliothek/src/App.java
+++ Bibliothek/src/App.java
@@ -1,19 +1,19 @@
 import java.awt.Canvas;
+//Wenn von Canvas geerbt wird wird immer paint() aufgerufen, weil das in Canvas definiert ist
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Point;
+import java.util.Random;
+
 import javax.swing.JFrame;
 
 public class App extends Canvas {
 
-    int x1;
-    int y1;
-    int x2;
-    int y2;
 
     public static void main(String[] args) {
 
-        App dr = new App(100, 100, 200, 200);
+        App dr = new App();
         JFrame frame = new JFrame("Eine Linie!");
         frame.add(dr);
         frame.pack();
@@ -22,24 +22,47 @@ public class App extends Canvas {
 
     @Override
     public Dimension getPreferredSize() {
-        return new Dimension(600, 600);
+        return new Dimension(600, 800);
     }
 
+    private void drawSlide(int x1, int y1, int Xlength, int Ylength, Graphics g){
+        
+        g.drawString("Rutsche", x1 + 20, y1 + 20);
+        
+        g.drawLine(x1, y1, x1 + Xlength, y1);
+        g.drawLine(x1, y1 + Ylength, x1 + Xlength, y1 + Ylength);
+        g.drawLine(x1, y1, x1, y1 + Ylength);
+        g.drawLine(x1 + Xlength, y1, x1 + Xlength, y1 + Ylength);
+
+        g.drawOval(x1 + 10, y1 + Ylength - 130, 120, 120);
+    
+        g.drawLine(x1 + 220, y1 + 50, x1 + 260, y1 + 100);
+        g.drawLine(x1 + 260, y1 + 100, x1 + 140, y1 + 150);
+        g.drawLine(x1 + 140, y1 + 150, x1 + 100, y1 + 100);
+        g.drawLine(x1 + 100, y1 + 100, x1 + 220, y1 + 50);
+    
+    }
+
+
+
+/*
+    private void drawSlide(Graphics g){
+        g.drawLine(50, 50, 150, 50);
+        g.drawLine(50, 100, 150, 100);
+        g.drawLine(50, 50, 50, 100);
+        g.drawLine(150, 50, 150, 100);
+    }
+    */
     @Override
     public void paint(Graphics g) {
         super.paint(g);
+        drawSlide(100, 100, 300, 200, g);
+        drawSlide(100, 400, 300, 200, g);
         
-        g.drawLine(x1, y1, x2, y2);
+// amelie am montag 19        g.drawLine(100, 100, 200, 200);
     }
 
-    App(int x1, int y1, int x2, int y2) {
-        this.x1 = x1;
-        this.y1 = y1;
-        this.x2 = x2;
-        this.y2 = y2;
-    }
 
-    
 }
 
 
